f3a875521841bf4ad133a1a5048c15acb15ff00d
assmuni <assmuni@gmail.com>
2019-01-20 11:30:15 +0800

    heroku setup

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ app.get('/', (req, res) => {
     res.json({message: 'connected'});
 });
 
-const portNumber = 3000;
-app.listen(process.env.PORT || portNumber, () => {
-    console.info(`server online at,, can you guest`);
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
